27600d2e1b373875ad93b3b28a96c2b76b0f54c6
yenarue <yenarue@gmail.com>
2020-11-06 18:33:31 +0900

    chore: 환경변수 관련 설정파일 추가 (dotenv, config)

---
+++ config.js
@@ -0,0 +1,15 @@
+const config = {};
+
+config.development = {
+  serverBaseUrl: process.env.VUE_APP_SERVER_BASE_URL || 'http://localhost:8080',
+  port: process.env.VUE_APP_PORT || 3000,
+};
+
+config.staging = config.development;
+
+config.production = {
+  serverBaseUrl : config.development.serverBaseUrl,
+  port: config.development.port,
+};
+
+module.exports = config[process.env.NODE_ENV];
